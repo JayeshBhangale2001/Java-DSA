75c92e3b03680e79747f68740d34c4bc8a1a1b9b
Jayesh <jayeshbhangale2001@gmail.com>
2023-11-13 22:49:35 +0530

    recursion1.java

---
+++ Recursion/recursion1.java
@@ -0,0 +1,18 @@
+public class recursion1 {
+    public static void print(int n) {
+        if (n < 0) {
+            return;
+        }
+        if (n == 0) {
+            System.out.println(n);
+            return;
+        }
+        print(--n);
+        System.out.println(n + " ");
+    }
+
+    public static void main(String[] args) {
+        int num = 3;
+        print(num);
+    }
+}
